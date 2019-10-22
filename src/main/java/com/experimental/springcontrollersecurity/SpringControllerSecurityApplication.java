4ca6aa70c95ea6da2161fe0f8848651222d7ebb6
Jan Mewes <jan.mewes@epages.com>
2019-10-22 20:29:39 +0200

    Initial commit

---
+++ src/main/java/com/experimental/springcontrollersecurity/SpringControllerSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.experimentalsoftware.springcontrollersecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringControllerSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringControllerSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/experimental/springcontrollersecurity/SpringControllerSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experimentalsoftware.springcontrollersecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringControllerSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
